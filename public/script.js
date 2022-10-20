ab029d87a55a34e278b57639831641f207d4112c
zaidsidd360 <zaidsidd360@gmail.com>
2022-10-21 02:39:57 +0530

    Added the rest of the logic and template

--- public/script.js
+++ public/script.js
@@ -1,27 +1,59 @@
-const resetBtn = document.getElementById("resetBtn")
+const resetBtn = document.getElementById("resetBtn");
 
 const doPasswordsMatch = (val1, val2) => {
-    if(val1 === val2) return true
-    else return false
-}
+  if (val1 === val2) return true;
+  else return false;
+};
 
 const isLessThanEight = (val1) => {
-    // check if password is less than 8 characters
-}
+  if (val1.length < 8) return true;
+  else return false;
+};
 
 const doesContainNum = (val1) => {
-    // check if password doesn't contain numbers
-}
+  let pattern = /[1-9]/g;
+  return val1.match(pattern);
+};
 
 resetBtn.addEventListener("click", () => {
-    const inputOneVal = document.getElementById("inputOne").value;
-    const inputTwoVal = document.getElementById("inputTwo").value;
+  const inputOne = document.getElementById("inputOne");
+  const inputTwo = document.getElementById("inputTwo");
+  let inputOneVal = inputOne.value;
+  let inputTwoVal = inputTwo.value;
 
-    let matching = doPasswordsMatch(inputOneVal, inputTwoVal);
-    const errorPara = document.getElementById("errorText");
-    if(matching === false) {
-        errorPara.innerHTML = "Passwords do not match!"
-    } 
-    
+  const mainContent = document.getElementById("main");
+  const afterReset = document.getElementById("afterReset");
 
-})
+  let matching = doPasswordsMatch(inputOneVal, inputTwoVal);
+  const matchPara = document.getElementById("matchPara");
+
+  let lessThanEight = isLessThanEight(inputOneVal);
+  const lengthPara = document.getElementById("lengthPara");
+
+  let containNum = doesContainNum(inputOneVal);
+  const numPara = document.getElementById("numPara");
+
+  if (matching === false) {
+    matchPara.innerHTML = "Passwords do not match.";
+  } else {
+    matchPara.style.display = "none";
+  }
+
+  if (lessThanEight === true) {
+    lengthPara.innerHTML = "Password length must be greater than 8.";
+  } else {
+    lengthPara.style.display = "none";
+  }
+
+  if (containNum === null) {
+    numPara.innerHTML = "Password must contain at least one number.";
+  } else {
+    numPara.style.display = "none";
+  }
+
+  if (matching === true && lessThanEight === false && containNum !== null) {
+    mainContent.style.display = "none";
+    afterReset.style.display = "flex";
+    afterReset.style.flexDirection = "column";
+  }
+});
